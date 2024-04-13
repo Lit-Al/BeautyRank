5fe4036ed472bae6999e7208abcfb0bba9e2c865
mrlom <mrlomka407@mail.ru>
2024-04-13 11:41:11 +0300

    change id metric`s

--- frontend/src/common/shared/ui/yandex-metrica/YandexMetrica.tsx
+++ frontend/src/common/shared/ui/yandex-metrica/YandexMetrica.tsx
@@ -1,34 +1,34 @@
 import Script from 'next/script';
 
 const YandexMetrika = () => {
-  return (
-    <>
-      <Script id="yandex-metrika" strategy="afterInteractive">
-        {`
+    return (
+        <>
+            <Script id="yandex-metrika" strategy="afterInteractive">
+                {`
           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
           m[i].l=1*new Date();
           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
           k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
           (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-          ym(96905454, "init", {
+          ym(97016084, "init", {
                 clickmap:true,
                 trackLinks:true,
                 accurateTrackBounce:true
           });
         `}
-      </Script>
-      <noscript>
-        <div>
-          <img
-            src="https://mc.yandex.ru/watch/96905454"
-            style={{ position: 'absolute', left: '-9999px' }}
-            alt=""
-          />
-        </div>
-      </noscript>
-    </>
-  );
+            </Script>
+            <noscript>
+                <div>
+                    <img
+                        src="https://mc.yandex.ru/watch/97016084"
+                        style={{position: 'absolute', left: '-9999px'}}
+                        alt=""
+                    />
+                </div>
+            </noscript>
+        </>
+    );
 };
 
 export default YandexMetrika;
