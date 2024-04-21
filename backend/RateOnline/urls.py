16cce836699bb746b6fc45f87336d19ed70d639b
mrlom <mrlomka407@mail.ru>
2024-04-21 19:06:46 +0300

    Убрал лишний маршрут статики и медиа (теперь s3)

--- backend/RateOnline/urls.py
+++ backend/RateOnline/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -32,5 +30,3 @@ urlpatterns = [
     path("api/v1/", include(api_urlpatterns_v1)),
 ]
 urlpatterns += swagger_urlpatterns
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
