7a75cfda05ad123e2f83f57cd723aafaaf0a28da
mrlom <mrlomka407@mail.ru>
2024-05-19 20:16:27 +0300

    change number of metric

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,39 +1,39 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import {Html, Head, Main, NextScript} from 'next/document';
 
 export default function Document() {
-  return (
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+    return (
+        <Html>
+            <Head/>
+            <body>
+            <Main/>
+            <NextScript/>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
                 (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
                 m[i].l=1*new Date();
                 for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
                 k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
                 (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
     
-                ym(97016084, "init", {
+                ym(97324622, "init", {
                     clickmap:true,
                     trackLinks:true,
                     accurateTrackBounce:true
                 });
           `,
-          }}
-        />
-        <noscript>
-          <div>
-            <img
-              src="https://mc.yandex.ru/watch/97016084"
-              style={{ position: 'absolute', left: '-9999px' }}
-              alt=""
+                }}
             />
-          </div>
-        </noscript>
-      </body>
-    </Html>
-  );
+            <noscript>
+                <div>
+                    <img
+                        src="https://mc.yandex.ru/watch/97324622"
+                        style={{position: 'absolute', left: '-9999px'}}
+                        alt=""
+                    />
+                </div>
+            </noscript>
+            </body>
+        </Html>
+    );
 }
